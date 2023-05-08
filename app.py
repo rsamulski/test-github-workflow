0576f85a4182ba98db07d544375dbd1ab240779c
rsamulski <85201028+rsamulski@users.noreply.github.com>
2023-05-08 18:27:01 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+"""
+WSB Flask app.
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """
+    Returns a greeting message.
+    """
+    return '<h1>Hello WSB! Greetings from Flask!</h1>'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
